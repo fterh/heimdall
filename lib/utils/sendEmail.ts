509e566d98c05492ed3e9b9bbee35cb9d1660b26
Fabian Terh <fabianterh@gmail.com>
2020-01-21 22:53:45 +0800

    Implement low-level module to send email through SMTP

--- lib/utils/sendEmail.ts
+++ lib/utils/sendEmail.ts
@@ -1,19 +1,27 @@
 import { SESV2 } from "aws-sdk";
 
-interface SendEmailOptions {
+export interface SendEmailOptions {
+  config?: SESV2.ClientConfiguration;
   from: string;
   to: Array<string>;
+  cc?: Array<string>;
   subject: string;
   body: string;
 }
 
+/**
+ * Sends an email using AWS's SDK.
+ * Use the sendEmailSMTP module for more advanced use cases,
+ * such as when the SMTP envelope should be different from the email headers.
+ */
 export default async (options: SendEmailOptions): Promise<void> => {
   console.log("Attempting to send response email");
-  const ses = new SESV2();
+  const ses = new SESV2(options.config);
   const sesParams: SESV2.SendEmailRequest = {
     FromEmailAddress: options.from,
     Destination: {
-      ToAddresses: options.to
+      ToAddresses: options.to,
+      CcAddresses: options.cc
     },
     Content: {
       Simple: {

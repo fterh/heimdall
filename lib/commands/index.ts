42a6d0a8bd53fbd6b70764dbcaf4fa886674393f
Fabian Terh <fabianterh@gmail.com>
2020-01-22 11:47:04 +0800

    Test command handler module

--- lib/commands/index.ts
+++ lib/commands/index.ts
@@ -9,31 +9,27 @@ import { Commands } from "../reserved";
 export default async (command: string, parsedMail: ParsedMail) => {
   if (parsedMail.from.value[0].address !== email) {
     throw new Error(
-      "Command email's sender is inconsistent with environment variable EMAIL. This should never happen."
+      "Command email's sender is inconsistent with environment variable EMAIL."
     );
   }
 
-  try {
-    switch (command) {
-      case Commands.Generate:
-        console.log("Invoking generate command");
-        await generate(parsedMail);
-        break;
+  switch (command) {
+    case Commands.Generate:
+      console.log("Invoking generate command");
+      await generate(parsedMail);
+      break;
 
-      case Commands.List:
-        console.log("Invoking list command");
-        await list();
-        break;
+    case Commands.List:
+      console.log("Invoking list command");
+      await list();
+      break;
 
-      case Commands.Remove:
-        await remove(parsedMail);
-        break;
+    case Commands.Remove:
+      await remove(parsedMail);
+      break;
 
-      default:
-        // This should never happen
-        throw new Error(`"${command}" is not a command`);
-    }
-  } catch (err) {
-    console.error(`An error occurred: ${err}`);
+    default:
+      // This should never happen
+      throw new Error(`"${command}" is not a command`);
   }
 };
---
+++ tests/lib/commands/index.test.ts
@@ -0,0 +1,48 @@
+import { ParsedMail } from "mailparser";
+import { email } from "../../../lib/env";
+import handleCommand from "../../../lib/commands";
+import generate from "../../../lib/commands/generate";
+import list from "../../../lib/commands/list";
+import remove from "../../../lib/commands/remove";
+import generateTestEmail from "../../utils/generateTestEmail";
+
+jest.mock("../../../lib/commands/generate");
+jest.mock("../../../lib/commands/list");
+jest.mock("../../../lib/commands/remove");
+
+let testEmailAuthenticated: ParsedMail, testEmailUnauthenticated: ParsedMail;
+beforeAll(async () => {
+  testEmailAuthenticated = await generateTestEmail({
+    from: email,
+    to: "command@domain.com"
+  });
+  testEmailUnauthenticated = await generateTestEmail({
+    from: "hostile@attacker.com",
+    to: "command@domain.com"
+  });
+});
+
+it("should call the right command handler", async () => {
+  await handleCommand("generate", testEmailAuthenticated);
+  expect(generate).toHaveBeenCalledTimes(1);
+
+  await handleCommand("list", testEmailAuthenticated);
+  expect(list).toHaveBeenCalledTimes(1);
+
+  await handleCommand("remove", testEmailAuthenticated);
+  expect(remove).toHaveBeenCalledTimes(1);
+});
+
+it("should throw if the command is unrecognized", async () => {
+  const res = handleCommand("edit", testEmailAuthenticated);
+  expect(res).rejects.toEqual(new Error(`"edit" is not a command`));
+});
+
+it("should throw if the command is not authenticated", async () => {
+  const res = handleCommand("somecommand", testEmailUnauthenticated);
+  expect(res).rejects.toEqual(
+    new Error(
+      "Command email's sender is inconsistent with environment variable EMAIL."
+    )
+  );
+});
